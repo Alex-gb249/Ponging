eab598383ced579f66d5ed6a1d1312da128ec2b1
Alex Bermúdez <alexis.gb249@gmail.com>
2026-06-20 22:56:04 -0500

    Feat: Add random direction and realistic collision

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,26 +1,33 @@
 extends Area2D
 
-const SPEED = 1.5
-var directionX = 1
-var directionY = 1
+const SPEED = 150
+var velocity = Vector2.ZERO
 
+func _ready() -> void:
+	randomize()
+	_start_ball()
+
+func _start_ball() -> void:
+	var angle = randf_range(-PI / 4, PI / 4)
+	var direction = -1 if randf() < 0.5 else 1
+	velocity = Vector2(cos(angle) * direction, sin(angle)).normalized() * SPEED
+	if abs(velocity.y) < 0.2:
+		velocity.y = 0.2 * sign(velocity.y) if velocity.y != 0 else 0.2
+		velocity = velocity.normalized() * SPEED
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_position.x += directionX * (SPEED + delta)
-	global_position.y += directionY * (SPEED + delta)
+	global_position += velocity * delta
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	# TODO: Change to use a score system
 	get_tree().change_scene_to_file("res://scenes/scenary.tscn")
 
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Players"):
-		directionX = directionX * -1
-
+		velocity.x = -velocity.x
+		velocity = velocity.normalized() * SPEED
 
 func _on_area_entered(area: Area2D) -> void:
-	print("Area entered:", area.name, area.get_groups())
 	if area.is_in_group("Walls"):
-		directionY = directionY * -1
+		velocity.y = -velocity.y
